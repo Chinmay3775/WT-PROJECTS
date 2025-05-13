6209f39afa97e70a3357d440707f3fdfc0029d02
Chinmay Keripale <161611108+Chinmay3775@users.noreply.github.com>
2025-05-14 00:14:17 +0530

    Random Number Generator

---
+++ Random Number Generator/script.js
@@ -0,0 +1,7 @@
+const generatebtn=document.getElementById("generate-btn");
+const numberElement=document.getElementById("random-number");
+
+generatebtn.addEventListener("click",()=>{
+    const randomNumber = Math.floor((Math.random()*100)+1);
+    numberElement.textContent= randomNumber;
+});
